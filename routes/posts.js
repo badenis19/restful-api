52dd9f0f51a5002cff2e1f7a5fd5f209a368f311
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-15 13:30:24 +0100

    view all, create, view one created but endpoint having issues

--- routes/posts.js
+++ routes/posts.js
@@ -2,28 +2,54 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post') // model, can use new post()
 
-router.get('/', (req, res) => {
-  res.send('We are on Posts')
+
+
+// VIEW ALL THE POSTS 
+router.get('/', async (req, res) => {
+  // console.log(res)
+  try {
+    const posts = await Post.find(); // Post.find() alone returns everything, use .limit(number) to filter result.
+    return res.json(posts)
+  } catch (err) {
+    return res.json({ message: err })
+  }
 });
 
-// To create new Post
-router.post('/', (req, res) => {
-  console.log(">>>", req.body);
-  // console.log(">>>", req);
+// CREATE NEW POST (CREATE)
+router.post('/', async (req, res) => {
+  // res.json({ test: "test"})
+  console.log(res)
   const post = new Post({
     title: req.body.title,
     description: req.body.description
   });
 
-  
-  post.save()
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.json({ message: err })
-      console.log(err)
-    });
+  try {
+    const savedPost = await post.save()
+    console.log(savedPost)
+    return res.json(savedPost)
+  } catch (err) {
+    console.log(err)
+    return res.json({ message: err })
+  }
+
 });
- 
+
+// // VIEW ONE POST 
+router.get("/:postId", async (req, res) => {
+  console.log("post")
+  try {
+    const postId = req.params.postId;
+    const post = await Post.findById(postId)
+    return res.json(post)
+  } catch (err) {
+    console.log(err)
+    res.json({ message: err })
+  }
+})
+
+
+
+
 module.exports = router;
+
